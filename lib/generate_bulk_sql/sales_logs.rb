8a9307e46f60e51d157af148526bc199314acb5e
ochi-sho-private-study <ochisyoji1118@gmail.com>
2024-04-07 16:39:22 +0900

    sales_logsのクエリ生成スクリプトをリファクタリングする
    他のテーブルのクエリ生成ロジックと共通化するため

--- lib/generate_bulk_sql/sales_logs.rb
+++ lib/generate_bulk_sql/sales_logs.rb
@@ -5,21 +5,52 @@ gemfile do
   gem 'faker'
 end
 
-# sales_logs テーブル用の Bulk Insert SQLクエリを生成し出力する関数
-def generate_and_print_sales_logs_sql(count)
-  batch_size = 1000 # 一度に挿入する最大のレコード数
+module SalesLogs
+  class QueryBuilder
+    attr_reader :query
 
-  (1..count).each_slice(batch_size) do |slice|
-    values = slice.map do
-      employee_roster_id = rand(1..100) # 例示のため、employee_roster_idは1から100の範囲でランダムに設定
-      sales_quantity = rand(1..100) # sales_quantityを1から100の範囲でランダムに設定
-      sales_date = Faker::Date.between(from: '2020-01-01', to: '2024-01-01').to_s # sales_dateを過去4年間でランダムに設定
-      "(#{employee_roster_id}, #{sales_quantity}, '#{sales_date}')"
-    end.join(", ")
+    def self.build(batch_size) = new(batch_size).build
+    def initialize(batch_size) = @batch_size = batch_size
+    def build = build_bulk_insert_query
 
-    sql = "INSERT INTO sales_logs (employee_roster_id, sales_quantity, sales_date) VALUES #{values};"
-    puts sql
+    def build_bulk_insert_query
+      "INSERT INTO sales_logs (employee_roster_id, sales_quantity, sales_date) VALUES #{build_values.join(", ")};"
+    end
+
+    private
+
+    attr_reader :batch_size
+
+    def build_values
+      (1..batch_size).map do
+        "(#{AttributeBuilder.build_employee_roster_id}, #{AttributeBuilder.build_sales_quantity}, '#{AttributeBuilder.build_sales_date}')"
+      end
+    end
+
+    module AttributeBuilder
+      class << self
+        def build_employee_roster_id = rand(1..100) # 例示のため、employee_roster_idは1から100の範囲でランダムに設定
+
+        def build_sales_quantity = rand(1..100) # sales_quantityを1から100の範囲でランダムに設定
+
+        def build_sales_date = Faker::Date.between(from: '2020-01-01', to: '2024-01-01').to_s # sales_dateを過去4年間でランダムに設定
+      end
+    end
+  end
+
+  module QueryPrinter
+    DEFAULT_BATCH_SIZE = 1000
+
+    def self.print_queries(total_count, batch_size = DEFAULT_BATCH_SIZE)
+      (1..total_count).step(batch_size) do |offset|
+        count = [batch_size, total_count - offset + 1].min
+        $stdout.puts QueryBuilder.build(count)
+      end
+    end
   end
 end
 
-generate_and_print_sales_logs_sql(1000000)
+if __FILE__ == $0
+  # ARGV[0]にはINSERTしたいレコード数が入る
+  SalesLogs::QueryPrinter.print_queries(ARGV[0].to_i)
+end
