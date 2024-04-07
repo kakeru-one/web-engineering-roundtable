cdd1c2402df5bd9dfe3a2470c09b564999f12607
ochi-sho-private-study <ochisyoji1118@gmail.com>
2024-04-07 16:17:17 +0900

    employee_rostersのbulk_insert用のクエリ発行スクリプトを拡張しやすいようにリファクタリングする

--- lib/generate_bulk_sql/employee_rosters.rb
+++ lib/generate_bulk_sql/employee_rosters.rb
@@ -3,25 +3,55 @@ require 'bundler/inline'
 gemfile do
   source 'https://rubygems.org'
   gem 'faker'
+  gem 'pry-byebug'
 end
 
-# Bulk Insert用のSQLクエリを生成し出力する関数
-def generate_and_print_bulk_insert_sql(count)
-  batch_size = 1000 # 一度に挿入する最大のレコード数
-  
-  # 全データを生成
-  all_data = (1..count).map do
-    name = Faker::Name.name.gsub("'", "''") # シングルクォートのエスケープ
-    prefecture_id = rand(1..47)
-    age = rand(10..70)
-    "('#{name}', #{prefecture_id}, #{age})"
+module EmployeeRosters
+  class QueryBuilder
+    attr_reader :query
+
+    def self.build(batch_size) = new(batch_size).build
+    def initialize(batch_size) = @batch_size = batch_size
+    def build = build_bulk_insert_query
+
+    def build_bulk_insert_query
+      "INSERT INTO employee_rosters (name, prefecture_id, age) VALUES #{build_values.join(", ")};"
+    end
+
+    private
+
+    attr_reader :batch_size
+
+    def build_values
+      (1..batch_size).map do
+        "('#{AttributeBuilder.build_name}', #{AttributeBuilder.build_prefecture_id}, #{AttributeBuilder.build_age})"
+      end
+    end
+
+    module AttributeBuilder
+      class << self
+        def build_name = Faker::Name.name.gsub("'", "''")
+
+        def build_prefecture_id = rand(1..47)
+
+        def build_age = rand(10..70)
+      end
+    end
   end
 
-  # 1000件ごとにデータをスライスして、各スライスに対してSQLクエリを生成・出力
-  all_data.each_slice(batch_size) do |data_slice|
-    sql = "INSERT INTO employee_rosters (name, prefecture_id, age) VALUES #{data_slice.join(", ")};"
-    puts sql
+  module QueryPrinter
+    DEFAULT_BATCH_SIZE = 1000
+
+    def self.print_queries(total_count, batch_size = DEFAULT_BATCH_SIZE)
+      (1..total_count).step(batch_size) do |offset|
+        count = [batch_size, total_count - offset + 1].min
+        $stdout.puts EmployeeRosterQueryBuilder.build(count)
+      end
+    end
   end
 end
 
-generate_and_print_bulk_insert_sql(10000)
+if __FILE__ == $0
+  # ARGV[0]にはINSERTしたいレコード数が入る
+  EmployeeRosters::QueryPrinter.print_queries(ARGV[0])
+end
